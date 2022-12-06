a51d6618060a4627c044adf2662e218637c64ced
Olga Bukina <o.utina2010@gmail.com>
2022-12-06 20:20:45 +0300

    HW_task29 added

---
+++ HW_task29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран. Вывод сделать отдельным методом. 5 -> [1, 2, 5, 7, 19, 3 -> [6, 1, 33]
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, 1, 100);
+WriteArray(numbers);
+
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
